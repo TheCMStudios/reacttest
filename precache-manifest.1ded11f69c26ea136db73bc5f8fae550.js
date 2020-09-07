843543b75b4169e940cacd77c98dd6e4b9794fdc
TheCMStudios <70780206+TheCMStudios@users.noreply.github.com>
2020-09-07 19:05:04 -0400

    First commit

---
+++ precache-manifest.1ded11f69c26ea136db73bc5f8fae550.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc513b007fec3f5959b311f5e3240e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b740d509c7a6f8465dc6",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "b276a4edf5956f3331b9",
+    "url": "/static/css/main.45da2178.chunk.css"
+  },
+  {
+    "revision": "b740d509c7a6f8465dc6",
+    "url": "/static/js/2.60d63f6d.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.60d63f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b276a4edf5956f3331b9",
+    "url": "/static/js/main.407ea85b.chunk.js"
+  },
+  {
+    "revision": "064f728010e43d89aefc",
+    "url": "/static/js/runtime-main.8f696bfe.js"
+  },
+  {
+    "revision": "43b1eab510bc8767e7e0725a6291fece",
+    "url": "/static/media/Banner-Stock-Photos-by-Megapixl.43b1eab5.jpg"
+  },
+  {
+    "revision": "dab916f83ef1ea6988aab463a7203e02",
+    "url": "/static/media/Wallpaper-4K-Full-HD-155-1024x768-Wallpaper-Baixar-Papel-.dab916f8.jpg"
+  },
+  {
+    "revision": "2e58b36cda6ba0fecbcc2fd9ee599891",
+    "url": "/static/media/background-1024x512.2e58b36c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
